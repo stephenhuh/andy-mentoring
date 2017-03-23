dffdd8c1768f1099ef191c8df5ea337daad13a16
Andy Ham <andyham613@gmail.com>
2017-03-22 20:11:35 -0500

    usda

--- iteration4/script.js
+++ iteration4/script.js
@@ -76,21 +76,60 @@ function handle(e){
         console.log(searchedname);
         var foodsobj = JSON.parse(response);
         var found = [];
+        var foundfood;
         for (var food in foodsobj) {
           if (foodsobj.hasOwnProperty(food)) {
             if (food === searchedname) {
+              foundfood = foodsobj[food];
               found.push(JSON.stringify(foodsobj[food]));
             }
           }
         }
+        console.log(foundfood);
+        console.log(found);
+        console.log(found.calories);
         if (found.length === 0) {
           document.getElementById("searchednameresult").innerHTML = searchedname +" not found in database"
         } else {
-          document.getElementById("searchednameresult").innerHTML = searchedname + " stats: "+found;
+          document.getElementById("searchednameresult").innerHTML = searchedname + " stats: " ;//+found;
+          document.getElementById("searchedcalories").innerHTML = foundfood.calories;
+          document.getElementById("searchedcarbs").innerHTML = foundfood.carbs +"g";
+          document.getElementById("searchedfats").innerHTML = foundfood.fats+"g";
+          document.getElementById("searchedproteins").innerHTML = foundfood.protein+"g";
+          tablefunction(foundfood);
         }
       })
     }
 	return false;
+}
+
+function tablefunction(foodobj){
+  var jsonObj = $.parseJSON(JSON.stringify(foodobj));
+  var html = '<table border="1">';
+  $.each(jsonObj, function(key, value){
+    html += '<tr>';
+    html += '<td>' + key + '</td>';
+    html += '<td>' + value + '</td>';
+    html += '</tr>';
+  });
+  html += '</table>';
+  document.getElementById("table1").innerHTML = html;
+  //$('table').html(html);
+};
+
+var hit_usda_api = function(){
+  $.get("http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=DEMO_KEY&nutrients=205&nutrients=204&nutrients=208&nutrients=269", function(response) {
+    //alert(response.report.foods);
+    console.log(response);
+    var foodnamearr = [];
+    var foodlist = response.report.foods
+    for (var i=0; i < foodlist.length; i++) {
+      foodnamearr.push(foodlist[i].name);
+    }
+    console.log(foodnamearr);
+    console.log(response.report.foods[0].nutrients[0].value);
+    document.getElementById("usdafoods").innerHTML = foodnamearr;
+  });
 }
 
 var getxproteins = function() {
